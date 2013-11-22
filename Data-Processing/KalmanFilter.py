1e8f6a58056aa7bb8edb3481fb50dfcecb30432d
drduggan <duggan629@gmail.com>
2013-11-22 13:18:46 -0600

    add alpha beta filter

--- Data-Processing/KalmanFilter.py
+++ Data-Processing/KalmanFilter.py
@@ -28,6 +28,73 @@ class AlphaBetaFilter():
     def __init__(self, data):
         self.data = data
 
+    def filter(self, alpha):
+        beta = 
+        gamma = 
+
+        filteredData = []
+
+        rawData = [208000.0, 207000.0, 217000.0, 204000.0, 216000.0, 229000.0, 229000.0, 242000.0, 310000.0, 241000.0, 245000.0, 247000.0, 259000.0, 257000.0, 299000.0, 245000.0, 255000.0]
+
+        Xp = None
+        Vp = None
+        Residual = None
+        Xs = None
+        Vs = None
+        As = None
+
+        Xs_0 = rawData[0]
+
+        data = rawData[1]
+        Xp = self.getXp(Xs_0, 0.0, 0.0)
+        Xs = rawData[1]
+        Vs = rawData[1] - rawData[0]
+        As = 0.0
+
+        filteredData.append(Xs)
+
+        for i in range(2, len(rawData)):
+            print "Iteration:", i
+            data = rawData[i]
+            print data
+            Xp = self.getXp(Xs, Vs, As)
+            print "Xp", Xp
+            Vp = self.getVp(Vs, As)
+            print "Vp:", Vp
+            residual = self.getResidual(data, Xp)
+            print "Residual", residual
+
+            Xs = self.getXs(Xp, residual, alpha)
+            print "Xs", Xs
+            Vs = self.getVs(Vp, residual, beta)
+            print "Vs", Vs
+            As = self.getAs(As, residual, gamma)
+            print "As", As
+            print "\n"
+
+            filteredData.append(Xs)
+
+        # for i in range(min(len(filteredData), len(rawData))):
+        #     print "Raw:", rawData[i], " | ", filteredData[i], " :FilteredData"
+
+    def getXp(self, Xs_minus, Vs_minus, As_minus):
+        return Xs_minus + Vs_minus + 0.5*As_minus
+
+    def getVp(self, Vs_minus, As_minus):
+        return Vs_minus + As_minus
+
+    def getResidual(self, data, Xp):
+        return data - Xp
+
+    def getXs(self, Xp, residual, alpha):
+        return Xp + alpha*residual
+
+    def getVs(self, Vp, residual, beta):
+        return Vp + beta*residual
+
+    def getAs(self, As_minus, residual, gamma):
+        return As_minus + gamma*residual
+
     def execute(self):
         dt = 0.5
         xk_1 = 0
@@ -57,6 +124,11 @@ class AlphaBetaFilter():
             print( "%f \t %f\n", xm, xk_1 )
             time.sleep( 1 )
 
-ab = AlphaBetaFilter([0])
-ab.execute()
-        
+ab = AlphaBetaFilter([1])
+ab.filter(0.2)        
+
+
+
+
+
+
