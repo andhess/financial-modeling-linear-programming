8727b834a61f6532d79edc34a78ce23fe737480f
andhess <andrewclaytonhess@gmail.com>
2013-12-08 23:27:52 -0600

    think my heuristic minimizer is done

--- Data-Processing/wienerFilter.py
+++ Data-Processing/wienerFilter.py
@@ -3,6 +3,7 @@ import scipy as sc
 import matplotlib as plt
 import random
 import time
+from operator import itemgetter
 
 class WienerPredictor():
     """ An implementation of the wiener filter. """
@@ -37,13 +38,41 @@ class WienerPredictor():
             return [ i[0] for i in w ]
 
         else:
-
-            wMinError()
+            return wMinError()
 
     def wMinError(self):
+        t = self.time - self.steps
+        alpha = [0] * self.steps
+        
+        for i in range(self.steps):
 
+            error = [0] * 11
+            for j in range(11):
 
+                predict = 0
+                for l in range(i):
+                    predict += alpha[i]*self.data[t+l]
 
+                predict += j/10.0 * self.data[t]
+                error[j] = math.pow( ( self.data[i+1] - predict ) , 2)
+
+            min1 = min( enumerate(error), key=itemgetter(1))[0]
+
+            error2 = [0] * 21
+            for k in range(21):
+                
+                predict = 0
+                for l in range(i):
+                    predict += alpha[i]*self.data[t+l]
+
+                predict += (min1/10.0 + (k - 11)/100.0) * self.data[t]
+                error[j] = math.pow( ( self.data[i+1] - predict ) , 2)
+
+            min2 = min( enumerate(error2), key=itemgetter(1))[0]
+
+            alpha[i] = min1/10.0 + (min2 - 11)/100.0
+
+        return alpha
 
     def constructRx(self):
 
